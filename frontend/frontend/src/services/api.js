f325af22a581e1e0e21a997a6d5caf94a684b8da
RENUPRIYA R <renupriya@aurostack.com>
2025-08-11 09:59:06 +0530

    Delete frontend directory

--- frontend/frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_BASE_URL = 'http://localhost:8081/api/products';
-
-export const getAllProducts = () => axios.get(API_BASE_URL);
-export const createProduct = (product) => axios.post(API_BASE_URL, product);
-export const updateProduct = (id, product) => axios.put(`${API_BASE_URL}/${id}`, product);
-export const deleteProduct = (id) => axios.delete(`${API_BASE_URL}/${id}`);
